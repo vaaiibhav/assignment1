5365ae5ac08f96fcf70932ec25a81522fed7c010
Vaaiibhav Vaidya <vaaiibhav@live.com>
2022-01-01 12:51:04 +0530

    added transition effects and BEM standardised
    added easeinout effect to transitions and front and back now in BEM standard

--- src/SocialCard.js
+++ src/SocialCard.js
@@ -4,21 +4,30 @@ import Location from './Location';
 import Phone from './Phone';
 import JobTitle from './JobTitle'
 
+const cardFlip = {
+    
+   }
+   
+
+function flipCard(){
+    
+}
+
 const SocialCard = (props) => {
   
     const { userData } = props;
     return (
         // <div className="card" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
         <div className="card">
-            <div className="card__body" >
+            <div className="card__body">
              
-                    <div className="front"> 
+                    <div className="card__front"> 
                         <div className="card__image"><img src={userData.avatar} /></div>
                         <div className="card__title">{userData.first_name} {userData.last_name}</div>
                         <JobTitle jobtitle={userData.employment} />
                     </div>
              
-                    <div className="back">  
+                    <div className="card__back">  
                     <Location location={userData.address} />
                     <Phone number={userData.phone_number} type="Phone Number" /></div>
             </div>
